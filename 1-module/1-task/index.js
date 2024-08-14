54622bdef7671b180974ed20efdc29d348b5d82a
Nikolay <bravicca@mail.ru>
2024-08-14 22:57:36 +0500

    1st task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,14 @@
 function factorial(n) {
-  // ваш код...
+  if (n < 0) {
+    return undefined;
+  }
+  let result = 1;
+  let i = 2;
+  while (i <= n) {
+    result *=i;
+    i++;
+  }
+  return result;
 }
+
+console.log ( factorial(3) );
