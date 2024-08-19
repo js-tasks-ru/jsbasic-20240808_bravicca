45fedadab152162e301e8717697f9c64763404ae
Nikolay <bravicca@mail.ru>
2024-08-19 20:53:28 +0500

    2-module

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,20 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let total = 0;
+  for (let key in salaries) {
+    let value = salaries [key];
+    if (typeof value === 'number' && isFinite (value) ) {
+      total +=value;
+    }
+  }
+      return total;// ваш код...
 }
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+}
+
+console.log (sumSalary (salaries) );
